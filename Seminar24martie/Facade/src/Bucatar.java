2bb76c9752eb5e2ccbfd083eae04173617f97270
Simona Dumitru <simonaionela.d@gmail.com>
2026-05-10 22:41:20 +0300

    update

---
+++ Seminar24martie/Facade/src/Bucatar.java
@@ -0,0 +1,17 @@
+public class Bucatar {
+    private String nume;
+    private boolean esteOcupat;
+
+    public Bucatar(String nume, boolean esteOcupat) {
+        this.nume = nume;
+        this.esteOcupat = esteOcupat;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public boolean isEsteOcupat() {
+        return esteOcupat;
+    }
+}
